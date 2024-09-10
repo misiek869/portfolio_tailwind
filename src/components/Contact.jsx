7d1659c6600234766d8ad062105ce6705a113314
Michal Gawik <gawlik.zator@o2.pl>
2024-09-10 14:03:11 +0200

    install react-toastify

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,11 +32,11 @@ const Contact = () => {
 			<SectionTitle text={'contact'} />
 			<form ref={form} onSubmit={sendEmail}>
 				<label>Name</label>
-				<input type='text' name='user_name' />
+				<input type='text' name='user_name' required />
 				<label>Email</label>
-				<input type='email' name='user_email' />
+				<input type='email' name='user_email' required />
 				<label>Message</label>
-				<textarea name='message' />
+				<textarea name='message' required />
 				<input type='submit' value='Send' />
 			</form>
 		</section>
