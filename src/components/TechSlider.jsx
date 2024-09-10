b0d5d7dc36e709e73e901dcfef02820e3b8b45dc
Michal Gawik <gawlik.zator@o2.pl>
2024-09-10 11:55:04 +0200

    add slider with icons

--- src/components/TechSlider.jsx
+++ src/components/TechSlider.jsx
@@ -1,7 +1,13 @@
 import Slider from 'react-slick'
-// import 'slick-carousel/slick/slick.css'
-// import 'slick-carousel/slick/slick-theme.css'
-import 
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import htmlIcon from '../assets/html-icon.svg'
+import cssIcon from '../assets/css-icon.svg'
+import jsIcon from '../assets/javascript-icon.svg'
+import tsIcon from '../assets/typescript-icon.svg'
+import reactIcon from '../assets/react-icon.svg'
+import nextIcon from '../assets/nextjs-icon.svg'
+import tailwindIcon from '../assets/tailwind-icon.svg'
 
 const TechSlider = () => {
 	var settings = {
@@ -89,27 +95,22 @@ const TechSlider = () => {
 	}
 
 	return (
-		<Slider {...settings}>
-			<img src={`/html-icon.svg`} alt='html Logo' className='h-12 w-12' />
-			<img src={`/css-icon.svg`} alt='css Logo' className='h-12 w-12' />
-			<img
-				src={`/javascript-icon.svg`}
-				alt='javascript Logo'
-				className='h-12 w-12'
-			/>
-			<img
-				src={`/typescript-icon.svg`}
-				alt='typescript Logo'
-				className='h-12 w-12'
-			/>
-			<img src={`/react-icon.svg`} alt='react Logo' className='h-12 w-12' />
-			<img src={`/nextjs-icon.svg`} alt='nextjs Logo' className='h-12 w-12' />
-			<img
-				src={`/tailwind-icon.svg`}
-				alt='tailwind Logo'
-				className='h-12 w-12'
-			/>
-		</Slider>
+		<div className='mt-24'>
+			<div className=' max-w-sm bottom-[70px] md:max-w-4xl xl:max-w-3xl px-5 lg:px-10 overflow-auto mx-auto bg-white rounded-md z-20 border-slate-200 border shadow-md'>
+				<div className='py-4 lg:py-8 cursor cursor-all-scroll'>
+					<Slider {...settings}>
+						<img src={htmlIcon} alt='html Logo' className='h-12 w-12' />
+						<img src={cssIcon} alt='css Logo' className='h-12 w-12' />
+						<img src={jsIcon} alt='javascript Logo' className='h-12 w-12' />
+
+						<img src={tsIcon} alt='typescript Logo' className='h-12 w-12' />
+						<img src={reactIcon} alt='react Logo' className='h-12 w-12' />
+						<img src={nextIcon} alt='nextjs Logo' className='h-12 w-12' />
+						<img src={tailwindIcon} alt='tailwind Logo' className='h-12 w-12' />
+					</Slider>
+				</div>
+			</div>
+		</div>
 	)
 }
 
