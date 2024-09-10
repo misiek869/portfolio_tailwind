2cdb11a9f907572dd7d87c24a71cf2e37d801391
Michal Gawik <gawlik.zator@o2.pl>
2024-09-10 08:49:36 +0200

    create styling

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
-const SectionTitle = () => {
-	return <div></div>
+const SectionTitle = ({ text }) => {
+	return (
+		<div className='border-b border-dark pb-5'>
+			<h2 className='text-3xl font-bold tracking-wider capitalize'>{text}</h2>
+		</div>
+	)
 }
 
 export default SectionTitle
