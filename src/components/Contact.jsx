af9d6da5e3b7da214e0f80a174745bddb1ddf5da
Michal Gawik <gawlik.zator@o2.pl>
2024-09-10 12:45:58 +0200

    install emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,10 @@
 import SectionTitle from './SectionTitle'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
 	return (
-		<section className='py-20 align-element' id='contact'>
+		<section className='py-20 align-element' id='skills'>
 			<SectionTitle text={'contact'} />
 		</section>
 	)
