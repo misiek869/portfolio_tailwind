d31e765adf0a47c82d990b86dad81998081743ba
Michal Gawik <gawlik.zator@o2.pl>
2024-09-10 12:56:12 +0200

    add env to gitingore

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,37 @@ import React, { useRef } from 'react'
 import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+	const form = useRef()
+
+	const sendEmail = e => {
+		e.preventDefault()
+
+		emailjs
+			.sendForm('service_bf4scjg', 'template_uuj0252', form.current, {
+				publicKey: 'vcTG7DbP8L8igeTvk',
+			})
+			.then(
+				() => {
+					console.log('SUCCESS!')
+				},
+				error => {
+					console.log('FAILED...', error.text)
+				}
+			)
+	}
+
 	return (
 		<section className='py-20 align-element' id='skills'>
 			<SectionTitle text={'contact'} />
+			<form ref={form} onSubmit={sendEmail}>
+				<label>Name</label>
+				<input type='text' name='user_name' />
+				<label>Email</label>
+				<input type='email' name='user_email' />
+				<label>Message</label>
+				<textarea name='message' />
+				<input type='submit' value='Send' />
+			</form>
 		</section>
 	)
 }
