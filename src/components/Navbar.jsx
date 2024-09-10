e80947aab6961da6d2b39767abdcd4c1a73d80f0
Michal Gawik <gawlik.zator@o2.pl>
2024-09-10 08:27:19 +0200

    add custom class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { links } from '../data'
 const Navbar = () => {
 	return (
 		<nav className='bg-cyan-700 '>
-			<div className='mx-auto max-w-7xl  px-8 py-4 flex flex-col  sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
+			<div className='align-element  px-8 py-4 flex flex-col  sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
 				<h2 className='text-3xl font-bold text-white'>
 					Web
 					<span className='text-orange-600'>Portfolio</span>
