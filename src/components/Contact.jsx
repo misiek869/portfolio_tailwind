0d1b19f65fab710ed6804eda8b6ba810c2f4ee44
Michal Gawik <gawlik.zator@o2.pl>
2024-09-10 12:12:43 +0200

    create component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import SectionTitle from './SectionTitle'
+
+const Contact = () => {
+	return (
+		<section className='py-20 align-element' id='contact'>
+			<SectionTitle text={'contact'} />
+		</section>
+	)
+}
+
+export default Contact
