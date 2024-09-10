ac99942be7fc97bbad01d494b5fc9ff0f1319693
Michal Gawik <gawlik.zator@o2.pl>
2024-09-10 09:02:41 +0200

    create card

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
-const SkillsCard = () => {
-	return <div></div>
+const SkillsCard = ({ icon, title, text }) => {
+	return (
+		<article className='flex flex-col items-center text-center'>
+			<span className='h-16 w-16'>{icon}</span>
+			<h4 className='mt-6 font-bold'>{title}</h4>
+			<p className='mt-2 text-slate-500'>{text}</p>
+		</article>
+	)
 }
 
 export default SkillsCard
