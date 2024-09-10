68436c9e135ee02d7daa279bc089d8fa92a83edf
Michal Gawik <gawlik.zator@o2.pl>
2024-09-10 08:11:00 +0200

    create component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+import heroImg from '../assets/michal.webp'
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa'
+
+const Hero = () => {
+	return <div></div>
+}
+
+export default Hero
