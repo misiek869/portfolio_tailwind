edfe3ddd0661e697562d2b0902bc38b2dcda7d9b
Michal Gawik <gawlik.zator@o2.pl>
2024-09-10 08:23:41 +0200

    create section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,35 @@
 import heroImg from '../assets/michal.webp'
-import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa'
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa'
 
 const Hero = () => {
-	return <div></div>
+	return (
+		<section className='bg-cyan-700 py-24'>
+			<div className='mx-auto max-w-7xl  px-8 grid md:grid-cols-2 items-center gap-8'>
+				<article>
+					<h1 className='text-7xl font-bold text-white tracking-wider'>
+						Jestem Michał
+					</h1>
+					<p className='mt-4 text-3xl text-white capitalize tracking-wide'>
+						Aspirujący Front-end developer
+					</p>
+					{/* <p className='mt-2 text-lg text-slate-700 capitalize tracking-wide'>
+         
+          </p> */}
+					<div className='flex gap-x-4 mt-4'>
+						<a href='#'>
+							<FaGithubSquare className='h-8 w-8 text-white hover:text-orange-700 duration-300' />
+						</a>
+						<a href='#'>
+							<FaLinkedin className='h-8 w-8 text-white hover:text-orange-700 duration-300' />
+						</a>
+					</div>
+				</article>
+				<article className='hidden md:block '>
+					<img src={heroImg} className='h-80 lg:h-96 ' />
+				</article>
+			</div>
+		</section>
+	)
 }
 
 export default Hero
